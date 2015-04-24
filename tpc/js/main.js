cc7729c3fec8b3321166e5d84c739e5228540c4d
Konstantinos Kakoulis <kostas@Kostass-iMac.lan>
2015-04-24 16:12:04 +0300

    TPC init

---
+++ tpc/js/main.js
@@ -0,0 +1,17 @@
+
+
+jQuery(function($){
+				
+	$.supersized({
+
+		// Functionality
+		slideshow   :   0,		//Slideshow on/off
+												   
+		// Components
+		slides 		:  	[			// Slideshow Images
+				// {image : 'http://localhost/clients/tako/img/textur-web.jpg', title : 'Image Credit: Maria Kazvan'}
+				{image : 'http://bootstrap4me.com/wagschal/img/textur-web.jpg', title : 'Image Credit: Maria Kazvan'}
+				
+		]
+	});
+});
