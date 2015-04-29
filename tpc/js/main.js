6ca103f38c82cf4c2d21ccd9b2bce0cf6ac91fe5
Konstantinos Kakoulis <kostas@Kostass-iMac.lan>
2015-04-29 17:01:43 +0300

    tcp neue korekturen

--- tpc/js/main.js
+++ tpc/js/main.js
@@ -9,9 +9,28 @@ jQuery(function($){
 												   
 		// Components
 		slides 		:  	[			// Slideshow Images
-				{image : 'http://localhost/clients/tako/tpc/img/hg.jpg', title : 'Image Credit: Maria Kazvan'}
-				// {image : 'http://bootstrap4me.com/tpc/img/hg.jpg', title : 'Image Credit: Maria Kazvan'}
+				// {image : 'http://localhost/clients/tako/tpc/img/hg.jpg', title : 'Image Credit: Maria Kazvan'}
+				{image : 'http://bootstrap4me.com/sandbox/tpc/img/hg.jpg', title : 'Image Credit: Maria Kazvan'}
 				
 		]
 	});
 });
+
+$(document).ready(function() {
+    function toggleNavbarMethod() {
+        if ($(window).width() > 768) {
+            $('.navbar .dropdown').on('mouseover', function(){
+                $('.dropdown-toggle', this).trigger('click'); 
+            }).on('mouseout', function(){
+                $('.dropdown-toggle', this).trigger('click').blur();
+            });
+        }
+        else {
+            $('.navbar .dropdown').off('mouseover').off('mouseout');
+        }
+    }
+    toggleNavbarMethod();
+    $(window).resize(toggleNavbarMethod);
+});
+
+
