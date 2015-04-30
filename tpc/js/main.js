0957e10b0c66f889a6b427d86a299932bc264e3c
Konstantinos Kakoulis <kostas@Kostass-iMac.lan>
2015-04-30 12:32:01 +0300

    more changes tin TPC

--- tpc/js/main.js
+++ tpc/js/main.js
@@ -16,6 +16,8 @@ jQuery(function($){
 	});
 });
 
+
+
 $(document).ready(function() {
     function toggleNavbarMethod() {
         if ($(window).width() > 768) {
@@ -34,3 +36,57 @@ $(document).ready(function() {
 });
 
 
+// $(function(){
+//     $(".dropdown").hover(            
+//             function() {
+//                 $('.dropdown-menu', this).stop( true, true ).fadeIn("fast");
+//                 $(this).toggleClass('open');
+//                 $('b', this).toggleClass("caret caret-up");                
+//             },
+//             function() {
+//                 $('.dropdown-menu', this).stop( true, true ).fadeOut("fast");
+//                 $(this).toggleClass('open');
+//                 $('b', this).toggleClass("caret caret-up");                
+//             });
+//     });
+
+
+// (function ($, window, delay) {
+//   var theTimer = 0;
+//   var theElement = null;
+//     var theLastPosition = {x:0,y:0};
+//   $('[data-toggle]')
+//     .closest('li')
+//     .on('mouseenter', function (inEvent) {
+//     if (theElement) theElement.removeClass('open');
+//     window.clearTimeout(theTimer);
+//     theElement = $(this);
+
+//     theTimer = window.setTimeout(function () {
+//       theElement.addClass('open');
+//     }, delay);
+//   })
+//     .on('mousemove', function (inEvent) {
+//         if(Math.abs(theLastPosition.x - inEvent.ScreenX) > 4 || 
+//            Math.abs(theLastPosition.y - inEvent.ScreenY) > 4)
+//         {
+//             theLastPosition.x = inEvent.ScreenX;
+//             theLastPosition.y = inEvent.ScreenY;
+//             return;
+//         }
+        
+//     if (theElement.hasClass('open')) return;
+//     window.clearTimeout(theTimer);
+//     theTimer = window.setTimeout(function () {
+//       theElement.addClass('open');
+//     }, delay);
+//   })
+//     .on('mouseleave', function (inEvent) {
+//     window.clearTimeout(theTimer);
+//     theElement = $(this);
+//     theTimer = window.setTimeout(function () {
+//       theElement.removeClass('open');
+//     }, delay);
+//   });
+// })(jQuery, window, 0); 
+// 200 is the delay in milliseconds
