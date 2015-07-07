79eb5f64cbbb154a8ac540e0a75466594155467b
Konstantinos Kakoulis <kostas@Kostass-iMac.local>
2015-07-07 20:22:01 +0300

    tpc small and old changes

--- tpc/js/main.js
+++ tpc/js/main.js
@@ -15,3 +15,42 @@ jQuery(function($){
 		]
 	});
 });
+
+
+
+// Open the Navbar Menu with hover
+$('.dropdown-menu').on({
+   mouseenter: function() {
+      console.log('enter');
+      if ( !$('.dropdown').hasClass('active') ) {
+        $('.dropdown').addClass('active');
+      }
+   },
+   mouseleave: function() {
+      var $self = $(this);
+      // setTimeout(function(){
+         console.log('bye!');
+         if ( $('.dropdown').hasClass('open') ) {
+          $('.dropdown').addClass('active');
+        }
+         else if ( $('.dropdown').hasClass('active') ) {
+          $('.dropdown').removeClass('active');
+        }
+        else {
+          $('.dropdown').removeClass('active');
+        }
+      // }, 800);
+
+   }
+})
+
+$( ".dropdown" ).click(function() {
+  if ( !$('.dropdown').hasClass('open') ) {
+    $('.dropdown').addClass('active');
+  }
+  else {
+    $('.dropdown').removeClass('active');
+  }
+});
+
+
