ad4d5cbbfdd16fea5d69ee2d67a57c68d5b93355
Konstantinos Kakoulis <kostas@Kostass-iMac.local>
2015-07-08 20:34:21 +0300

    work on Navigation

--- anders/js/main.js
+++ anders/js/main.js
@@ -1,1 +1,16 @@
 
+jQuery(function($){
+
+   $.supersized({
+
+      // Functionality
+      slideshow   :   0,      //Slideshow on/off
+
+      // Components
+      slides      :     [        // Slideshow Images
+            {image : 'http://localhost/clients/tako/anders/img/hg.jpg', title : 'Image Credit: Maria Kazvan'}
+            // {image : 'http://bootstrap4me.com/sandbox/tpc/img/hg.jpg', title : 'Image Credit: Maria Kazvan'}
+
+      ]
+   });
+});
