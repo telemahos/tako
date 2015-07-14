4b514a9d1a79506d5d61c3d80ba6fd24d393b4c6
telemahos <kakoulis@hotmail.com>
2015-07-14 19:19:09 +0300

    Front and Mpbile Ready

--- anders/js/main.js
+++ anders/js/main.js
@@ -9,7 +9,7 @@ jQuery(function($){
       // Components
       slides      :     [        // Slideshow Images
             {image : 'http://localhost/clients/tako/anders/img/hg.jpg', title : 'Image Credit: Maria Kazvan'}
-            // {image : 'http://bootstrap4me.com/sandbox/tpc/img/hg.jpg', title : 'Image Credit: Maria Kazvan'}
+            // {image : 'http://bootstrap4me.com/sandbox/anders/img/hg.jpg', title : 'Image Credit: Maria Kazvan'}
 
       ]
    });
