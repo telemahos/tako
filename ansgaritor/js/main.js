df6cf36a6acc61594d32d2dfe01d63e9746075c3
Kostas Kakoulis <kakoulis@hotmail.com>
2015-07-16 16:55:49 +0300

    ansgaritor Project almost ready

---
+++ ansgaritor/js/main.js
@@ -0,0 +1,17 @@
+
+
+jQuery(function($){
+				
+	$.supersized({
+
+		// Functionality
+		slideshow   :   0,		//Slideshow on/off
+												   
+		// Components
+		slides 		:  	[			// Slideshow Images
+				{image : 'http://localhost/clients/tako/ansgaritor/img/background1.jpg', title : 'Image Credit: Maria Kazvan'}
+				// {image : 'http://bootstrap4me.com/sanbox/ansgaritor/img/background1.jpg', title : 'Image Credit: Maria Kazvan'}
+				
+		]
+	});
+});
