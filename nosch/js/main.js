0e18062e87adadc6452ef765edf83ac5484268dc
Konstantinos Kakoulis <kakoulis73@GMAIL.COM>
2015-09-25 12:07:34 +0300

    Nosch Fertig- Abgegeben

--- nosch/js/main.js
+++ nosch/js/main.js
@@ -1,7 +1,24 @@
 
+// $(document).ready(function() {
+//    function toggleNavbarMethod() {
+//        if ($(window).width() > 759) {
+//            $('.navbar .dropdown').on('mouseover', function(){
+//                $('.dropdown-toggle', this).trigger('click');
+//            }).on('mouseout', function(){
+//                $('.dropdown-toggle', this).trigger('click').blur();
+//            });
+//        }
+//        else {
+//            $('.navbar .dropdown').off('mouseover').off('mouseout');
+//        }
+//    }
+//    toggleNavbarMethod();
+//    $(window).resize(toggleNavbarMethod);
+// }); 
+
 $(document).ready(function() {
    function toggleNavbarMethod() {
-       if ($(window).width() > 709) {
+       if ($(window).width() > 768) {
            $('.navbar .dropdown').on('mouseover', function(){
                $('.dropdown-toggle', this).trigger('click');
            }).on('mouseout', function(){
