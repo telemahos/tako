d44fd834a4b3fd1dfb67f5cd4fc79147289c8492
Konstantinos Kakoulis <kostas@Kostass-iMac.local>
2016-02-25 22:31:24 +0200

    init HundB Project

---
+++ hundb/js/main.js
@@ -0,0 +1,55 @@
+
+$(document).ready(function() {
+   function toggleNavbarMethod() {
+       if ($(window).width() > 768) {
+           $('.navbar .dropdown').on('mouseover', function(){
+               if($('li.dropdown').hasClass( "open" )){
+                  // $('.dropdown-toggle', this).trigger('click');
+               }
+               else {
+                  $('.dropdown-toggle', this).trigger('click');
+               }
+           }).on('mouseout', function(){
+               $('.dropdown-toggle', this).trigger('click').blur();
+           });
+       }
+       else {
+           $('.navbar .dropdown').off('mouseover').off('mouseout');
+       }
+   }
+   toggleNavbarMethod();
+   $(window).resize(toggleNavbarMethod);
+});
+
+
+// Open the Navbar Menu with hover
+// $('.dropdown-menu').on({
+//    mouseenter: function() {
+//       console.log('enter');
+//       if ( !$('.dropdown').hasClass('active') ) {
+//         $('.dropdown').addClass('active');
+//       }
+//    },
+//    mouseleave: function() {
+//       var $self = $(this);
+//          console.log('bye!');
+//          if ( $('.dropdown').hasClass('open') ) {
+//           $('.dropdown').addClass('active');
+//         }
+//          else if ( $('.dropdown').hasClass('active') ) {
+//           $('.dropdown').removeClass('active');
+//         }
+//         else {
+//           $('.dropdown').removeClass('active');
+//         }
+//    }
+// })
+
+// $( ".dropdown" ).click(function() {
+//   if ( !$('.dropdown').hasClass('open') ) {
+//     $('.dropdown').addClass('active');
+//   }
+//   else {
+//     $('.dropdown').removeClass('active');
+//   }
+// });
